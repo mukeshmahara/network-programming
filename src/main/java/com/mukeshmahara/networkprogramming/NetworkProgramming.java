f917a88b3032be6f6e5408f51a04c5e5d3964def
mukeshmahara <mukeshmahara1@gmail.com>
2023-03-08 13:53:45 +0545

    Source file added

---
+++ src/main/java/com/mukeshmahara/networkprogramming/NetworkProgramming.java
@@ -0,0 +1,15 @@
+///*
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+// */
+package com.mukeshmahara.networkprogramming;
+
+/**
+ *
+ * @author mukesh
+ */
+public class NetworkProgramming {
+    public static void main(String[] args) {
+        System.out.println("Network Programming");
+    }
+}
