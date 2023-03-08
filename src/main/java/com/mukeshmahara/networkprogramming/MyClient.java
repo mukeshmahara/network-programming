012f9195b2a8d153a757b9add49469f55ce4e5d5
mukeshmahara <mukeshmahara1@gmail.com>
2023-03-08 14:20:04 +0545

    Minor fixes for exception error message

--- src/main/java/com/mukeshmahara/networkprogramming/MyClient.java
+++ src/main/java/com/mukeshmahara/networkprogramming/MyClient.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mukeshmahara.networkprogramming;
 
 import java.io.DataInputStream;
@@ -21,7 +18,7 @@ public class MyClient {
             DataOutputStream dout = new DataOutputStream(s.getOutputStream());
             DataInputStream din = new DataInputStream(s.getInputStream());
 
-            String str = (String) din.readUTF(); //return utf to string
+            String str = din.readUTF(); //return utf to string
             System.out.println("Server Says= " + str);
 
             dout.writeUTF("Hello Server");
@@ -30,7 +27,8 @@ public class MyClient {
             dout.close();
             s.close();
         } catch (Exception e) {
-            System.out.println(e);
+//            System.out.println(e);
+            System.out.println("Error :"+ e.getMessage());
         }
     }
 }
