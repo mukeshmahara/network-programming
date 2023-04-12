d72fd6ac771e1faab66954d0c432477177b8d530
mukeshmahara <mukeshmahara1@gmail.com>
2023-04-12 07:26:16 +0545

    Reading URL headers

---
+++ src/main/java/URLConnections/ReadingURLHeaders.java
@@ -0,0 +1,27 @@
+package URLConnections;
+
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Map;
+
+public class ReadingURLHeaders {
+    public static void main(String[] args) {
+        URL url = null;
+        try {
+            url = new URL("http://www.google.com");
+            URLConnection connection = url.openConnection( );
+            //Listing all the headers
+            Map headers = connection.getHeaderFields();
+            for (Object header : headers.entrySet()) {
+                System.out.println(header);
+            }
+        }catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }catch (IOException e){
+            throw new RuntimeException();
+        }
+
+    }
+}
