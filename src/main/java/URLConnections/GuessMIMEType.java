05e48773a6aee5337ef01e1d1ee27440f6cd4e01
mukeshmahara <mukeshmahara1@gmail.com>
2023-04-21 06:32:13 +0545

    Guessing mime type using stream

---
+++ src/main/java/URLConnections/GuessMIMEType.java
@@ -0,0 +1,28 @@
+package URLConnections;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class GuessMIMEType {
+    public static void main(String[] args) {
+
+        try {
+            URL url = new URL("https://www.youtube.com");
+            URLConnection uc = url.openConnection();
+            uc.connect();
+            System.out.println("url Connection created...");
+            InputStream in = uc.getInputStream();
+            BufferedInputStream buffer = new BufferedInputStream(in);
+            System.out.println(
+                    URLConnection.guessContentTypeFromStream(buffer)
+
+            );
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
