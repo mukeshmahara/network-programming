dc344967d86336cf223f22598ff048c0f52b88cf
mukeshmahara <mukeshmahara1@gmail.com>
2023-06-01 05:01:19 +0545

    Serving binary data example

--- src/main/java/SocketForServers/ServingBinaryData.java
+++ src/main/java/SocketForServers/ServingBinaryData.java
@@ -8,39 +8,35 @@ import java.util.Date;
 
 public class ServingBinaryData {
 	public final static int PORT = 8000;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		long differenceBetweenEpochs = 2208988800L;
 		try (ServerSocket server = new ServerSocket(PORT)) {
-		while (true) {
+			while (true) {
 
-			try (Socket connection = server.accept()) {
+				try (Socket connection = server.accept()) {
 
-		OutputStream out = connection.getOutputStream();
-		Date now = new Date();
-		long msSince1970 = now.getTime();
-		long secondsSince1970 = msSince1970/1000;
-		long secondsSince1900 = secondsSince1970
-		+ differenceBetweenEpochs;
-		byte[] time = new byte[4];
-		time[0]
-		= (byte) ((secondsSince1900 & 0x00000000FF000000L) >> 24);
-		time[1]
-		= (byte) ((secondsSince1900 & 0x0000000000FF0000L) >> 16);
-		time[2]
-		= (byte) ((secondsSince1900 & 0x000000000000FF00L) >> 8);
-		time[3] = (byte) (secondsSince1900 & 0x00000000000000FFL);
-		out.write(time);
-		out.flush();
+					OutputStream out = connection.getOutputStream();
+					byte[] test = new byte[40];
+					
+					test = "Hello".getBytes();
+					
+					for (byte b : test) {
+						System.out.println((char) b);
+						
+					}
+
+					out.write(test);
+					out.flush();
+				} catch (IOException ex) {
+					System.err.println(ex.getMessage());
+				}
+			}
 		} catch (IOException ex) {
-		System.err.println(ex.getMessage());
+			System.err.println(ex);
 		}
-		}
-		} catch (IOException ex) {
-		System.err.println(ex);
-		}
-		
+
 	}
 
 }
